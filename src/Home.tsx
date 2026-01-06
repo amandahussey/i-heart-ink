19dff25f4c329a6d18458be0d5af4c836b8d43ed
Amanda Hussey <amanda.k.hussey@gmail.com>
2026-01-05 18:52:17 -0700

    basic nav - pending new font and styling

---
+++ src/Home.tsx
@@ -0,0 +1,11 @@
+import { Stack, Typography } from "@mui/material";
+
+const Home = () => {
+  return (
+    <Stack alignItems="center">
+      <Typography>Home Page</Typography>
+    </Stack>
+  );
+};
+
+export default Home;
---
+++ src/theme.ts
@@ -0,0 +1,19 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#99D4D4",
+      dark: "#01037B",
+    },
+    secondary: {
+      main: "#FF9C00",
+      dark: "#FF5A22",
+    },
+  },
+  typography: {
+    fontFamily: ["Delius", "Helvetica", "Arial", "serif"].join(","),
+    fontSize: 18,
+  },
+});
