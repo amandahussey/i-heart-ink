fea45bd5fab0838a83813f84e6d5cb4692877e0f
Amanda Hussey <amanda.k.hussey@gmail.com>
2026-01-08 15:28:59 -0700

    more responsive on large screens

--- src/Home.tsx
+++ src/Home.tsx
@@ -12,7 +12,12 @@ const Home = () => {
 
   return (
     <Stack alignItems="center">
-      <Box height="100vh">
+      <Box
+        height="100vh"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
         <img
           src={iHeartInk}
           alt="I Heart Ink Logo"
