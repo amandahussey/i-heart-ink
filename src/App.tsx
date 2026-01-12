b841adec9a15c196cb8fc0a9b0029c9a3e07ee9b
Amanda Hussey <amanda.k.hussey@gmail.com>
2026-01-12 10:32:57 -0700

    separate LightEffect component

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,7 @@ import Nav from "./Nav";
 import ScrollToTop from "./ScrollToTop";
 import Home from "./Home";
 import { NavContext } from "./NavContext";
-
-import "./App.css";
+import LightEffect from "./LightEffect";
 
 function App() {
   const isSm = useMediaQuery(theme.breakpoints.down("sm"));
@@ -27,36 +26,9 @@ function App() {
             width="100%"
             style={{ background: "black", color: "#E6FFFE", paddingBottom: 40 }}
           >
-            <div className="container">
-              <div
-                className="streak-big streak-blue"
-                style={{ top: "10%", left: "10%" }}
-              ></div>
-              <div
-                className="streak-small streak-blue"
-                style={{ top: "20%", left: isSm ? "20%" : "15%" }}
-              ></div>
-              <div
-                className="streak-small streak-blue"
-                style={{ top: "15%", left: isSm ? "25%" : "20%" }}
-              ></div>
-              <div
-                className="streak-small streak-blue"
-                style={{ top: "10%", right: isSm ? "25%" : "20%" }}
-              ></div>
-              <div
-                className="streak-small streak-blue"
-                style={{ top: "20%", right: isSm ? "20%" : "15%" }}
-              ></div>
-              <div
-                className="streak-big streak-blue"
-                style={{ top: "15%", right: "10%" }}
-              ></div>
-            </div>
-
+            <LightEffect />
             <Nav />
             <ScrollToTop />
-
             <Routes>
               {/* <Route path="/artist" element={<About />} />
             <Route path="/booking" element={<Booking />} />
---
+++ src/LightEffect.tsx
@@ -0,0 +1,39 @@
+import { useMediaQuery } from "@mui/material";
+import { theme } from "./theme";
+
+import "./LightEffect.css";
+
+function LightEffect() {
+  const isSm = useMediaQuery(theme.breakpoints.down("sm"));
+
+  return (
+    <div className="container">
+      <div
+        className="streak-big streak-blue"
+        style={{ top: "10%", left: "10%" }}
+      ></div>
+      <div
+        className="streak-small streak-blue"
+        style={{ top: "20%", left: isSm ? "20%" : "15%" }}
+      ></div>
+      <div
+        className="streak-small streak-blue"
+        style={{ top: "15%", left: isSm ? "25%" : "20%" }}
+      ></div>
+      <div
+        className="streak-small streak-blue"
+        style={{ top: "10%", right: isSm ? "25%" : "20%" }}
+      ></div>
+      <div
+        className="streak-small streak-blue"
+        style={{ top: "20%", right: isSm ? "20%" : "15%" }}
+      ></div>
+      <div
+        className="streak-big streak-blue"
+        style={{ top: "15%", right: "10%" }}
+      ></div>
+    </div>
+  );
+}
+
+export default LightEffect;
